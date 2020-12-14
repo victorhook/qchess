a1cf4ab3a324d5ff0a6e2f85d7ec5cfb32205b8b
Victor <victorkrook96@gmail.com>
2020-12-14 18:34:00 +0100

    Made simple tools to display and delete running engines

---
+++ tools/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROGRAM=engine_server.py
+pids=$(ps -aux | grep $PROGRAM | awk '{print $2}' $processes)
+
+for pid in $pids
+do
+    if [[ -d /proc/$pid ]]
+    then
+        kill $pid
+    fi;
+done
